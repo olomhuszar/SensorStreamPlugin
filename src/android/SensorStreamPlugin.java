481678ed943798738726e0af7a61693a573cbe87
Andras Baranko <olomhuszar@gmail.com>
2014-05-14 19:10:18 +0200

    Stream generic accelearation instead of linear

--- src/android/SensorStreamPlugin.java
+++ src/android/SensorStreamPlugin.java
@@ -18,7 +18,6 @@ import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -26,8 +25,6 @@ import java.net.UnknownHostException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-
-
 
 public class SensorStreamPlugin extends CordovaPlugin {
 	private static final String ACTION_START_STREAM = "startStream";
@@ -44,7 +41,6 @@ public class SensorStreamPlugin extends CordovaPlugin {
 	private boolean running = false;
 	private SensorManager mSensorManager;
 
-
 	@Override
 	public boolean execute(String action, JSONArray args,
 			final CallbackContext callbackContext) throws JSONException {
@@ -54,49 +50,50 @@ public class SensorStreamPlugin extends CordovaPlugin {
 				ipAddress = arguments.getString("ipAddress");
 				port = arguments.getInt("port");
 				cordova.getThreadPool().execute(new Runnable() {
-				    public void run() {
+					public void run() {
 						connect();
-						Log.d("CordovaLog", "Connecting to " + ipAddress + ":" + port);
-						callbackContext.success(); 
-				    }
+						Log.d("CordovaLog", "Connecting to " + ipAddress + ":"
+								+ port);
+						callbackContext.success();
+					}
 				});
 				return true;
 			} else if (ACTION_DISCONNECT.equals(action)) {
 				cordova.getThreadPool().execute(new Runnable() {
-				    public void run() {
+					public void run() {
 						disconnect();
 						Log.d("CordovaLog", "Disconnect from server");
-						callbackContext.success(); 
-				    }
+						callbackContext.success();
+					}
 				});
 				return true;
 			} else if (ACTION_START_STREAM.equals(action)) {
 				cordova.getThreadPool().execute(new Runnable() {
-				    @Override
+					@Override
 					public void run() {
 						startStream();
 						Log.d("CordovaLog", "Starting stream");
-						callbackContext.success(); 
-				    }
+						callbackContext.success();
+					}
 				});
 				return true;
 			} else if (ACTION_STOP_STREAM.equals(action)) {
 				cordova.getThreadPool().execute(new Runnable() {
-				    @Override
+					@Override
 					public void run() {
 						Log.d("CordovaLog", "Stop streaming");
-						callbackContext.success(); 
-				    }
+						callbackContext.success();
+					}
 				});
 				return true;
 			} else if (ACTION_GET_INFO.equals(action)) {
 				cordova.getThreadPool().execute(new Runnable() {
-				    @Override
+					@Override
 					public void run() {
 						String info = getInfo();
 						Log.d("CordovaLog", "Get sensor informations.");
-						callbackContext.success( info ); 
-				    }
+						callbackContext.success(info);
+					}
 				});
 				return true;
 			}
@@ -108,7 +105,8 @@ public class SensorStreamPlugin extends CordovaPlugin {
 			return false;
 		}
 	}
-	public void connect() {		
+
+	public void connect() {
 		try {
 			socket = new Socket(ipAddress, port);
 		} catch (UnknownHostException e) {
@@ -117,47 +115,70 @@ public class SensorStreamPlugin extends CordovaPlugin {
 			e.printStackTrace();
 		}
 	}
+
 	public void disconnect() {
-		if(socket != null) {
+		if (socket != null) {
 			try {
 				socket.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}	
+			}
 		}
 	}
+
 	public void startStream() {
-		if( out == null && socket != null ) {
+		if (out == null && socket != null) {
 			try {
 				out = new PrintWriter(socket.getOutputStream(), true);
-				mSensorManager = (SensorManager) cordova.getActivity().getSystemService(Context.SENSOR_SERVICE);
-				Sensor mLinerAcceleration  = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-				mSensorManager.registerListener(new LinerAccelerationListener(), mLinerAcceleration, SensorManager.SENSOR_DELAY_FASTEST);
+				mSensorManager = (SensorManager) cordova.getActivity()
+						.getSystemService(Context.SENSOR_SERVICE);
+				Sensor mLinearAcceleration = mSensorManager
+						.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+				mSensorManager.registerListener(
+						new LinearAccelerationListener(), mLinearAcceleration,
+						SensorManager.SENSOR_DELAY_FASTEST);
+				Sensor mGravity = mSensorManager
+						.getDefaultSensor(Sensor.TYPE_GRAVITY);
+				mSensorManager.registerListener(
+						new GravityListener(), mGravity,
+						SensorManager.SENSOR_DELAY_FASTEST);
+				Sensor mMagneticField = mSensorManager
+						.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+				mSensorManager.registerListener(
+						new MagneticFieldListener(), mMagneticField,
+						SensorManager.SENSOR_DELAY_FASTEST);
+				Sensor mRotationVector = mSensorManager
+						.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+				mSensorManager.registerListener(
+						new RotationVectorListener(), mRotationVector,
+						SensorManager.SENSOR_DELAY_FASTEST);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		running = true;
-		//streamData();
+		// streamData();
 	}
+
 	public void stopStream() {
 		running = false;
-		if(out != null) {
+		if (out != null) {
 			out.close();
 		}
 	}
+
 	protected class Stream implements Runnable {
 		@Override
 		public void run() {
 			double data;
 			Random rand = new Random();
-			while(running) {
-	        	data = 50.0 + ( rand.nextDouble() * 100 );
-	        	out.println(data);
+			while (running) {
+				data = 50.0 + (rand.nextDouble() * 100);
+				out.println(data);
 				try {
-					Thread.sleep(rand.nextInt(30)+20);
+					Thread.sleep(rand.nextInt(30) + 20);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -165,14 +186,18 @@ public class SensorStreamPlugin extends CordovaPlugin {
 			}
 		}
 	}
+
 	public void streamData() {
 		streamThread = new Thread(new Stream());
 		streamThread.start();
 	}
+
 	public String getInfo() {
 		StringBuilder builder = new StringBuilder("<ul>");
-		mSensorManager = (SensorManager) cordova.getActivity().getSystemService(Context.SENSOR_SERVICE);
-		List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+		mSensorManager = (SensorManager) cordova.getActivity()
+				.getSystemService(Context.SENSOR_SERVICE);
+		List<Sensor> deviceSensors = mSensorManager
+				.getSensorList(Sensor.TYPE_ALL);
 		for (Sensor sensor : deviceSensors) {
 			builder.append("<li>");
 			builder.append(sensor.getName());
@@ -195,8 +220,10 @@ public class SensorStreamPlugin extends CordovaPlugin {
 		builder.append("</ul>");
 		return builder.toString();
 	}
-	protected class LinerAccelerationListener implements SensorEventListener {
+
+	protected class LinearAccelerationListener implements SensorEventListener {
 		float prevNs;
+
 		@Override
 		public void onAccuracyChanged(Sensor sensor, int accuracy) {
 			Log.d("CordovaLog", "Sensor accuracy changed to: " + accuracy);
@@ -205,18 +232,20 @@ public class SensorStreamPlugin extends CordovaPlugin {
 		@Override
 		public void onSensorChanged(SensorEvent event) {
 			Log.d("CordovaLog", "new datas from sensor!");
+			String type = "Acceleration";
 			float x = event.values[0];
 			float y = event.values[1];
 			float z = event.values[2];
 			float ns = event.timestamp;
-			float deltat = ns-prevNs;
-			if(prevNs == 0) {
+			float deltat = ns - prevNs;
+			if (prevNs == 0) {
 				deltat = 0;
 			}
 			prevNs = ns;
-			if( running ) {
+			if (running) {
 				JSONObject result = new JSONObject();
 				try {
+					result.put("type", type);
 					result.put("x", x);
 					result.put("y", y);
 					result.put("z", z);
@@ -224,9 +253,120 @@ public class SensorStreamPlugin extends CordovaPlugin {
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
-	        	out.println(result.toString());
+				out.println(result.toString());
 			}
 		}
-		
+
+	}
+	protected class GravityListener implements SensorEventListener {
+		float prevNs;
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			Log.d("CordovaLog", "Sensor accuracy changed to: " + accuracy);
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			Log.d("CordovaLog", "new datas from sensor!");
+			String type = "Gravity";
+			float x = event.values[0];
+			float y = event.values[1];
+			float z = event.values[2];
+			float ns = event.timestamp;
+			float deltat = ns - prevNs;
+			if (prevNs == 0) {
+				deltat = 0;
+			}
+			prevNs = ns;
+			if (running) {
+				JSONObject result = new JSONObject();
+				try {
+					result.put("type", type);
+					result.put("x", x);
+					result.put("y", y);
+					result.put("z", z);
+					result.put("deltat", deltat);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				out.println(result.toString());
+			}
+		}
+
+	}
+	protected class MagneticFieldListener implements SensorEventListener {
+		float prevNs;
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			Log.d("CordovaLog", "Sensor accuracy changed to: " + accuracy);
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			Log.d("CordovaLog", "new datas from sensor!");
+			String type = "MagneticField";
+			float x = event.values[0];
+			float y = event.values[1];
+			float z = event.values[2];
+			float ns = event.timestamp;
+			float deltat = ns - prevNs;
+			if (prevNs == 0) {
+				deltat = 0;
+			}
+			prevNs = ns;
+			if (running) {
+				JSONObject result = new JSONObject();
+				try {
+					result.put("type", type);
+					result.put("x", x);
+					result.put("y", y);
+					result.put("z", z);
+					result.put("deltat", deltat);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				out.println(result.toString());
+			}
+		}
+
+	}
+	protected class RotationVectorListener implements SensorEventListener {
+		float prevNs;
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			Log.d("CordovaLog", "Sensor accuracy changed to: " + accuracy);
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			Log.d("CordovaLog", "new datas from sensor!");
+			String type = "RotationVector";
+			float x = event.values[0];
+			float y = event.values[1];
+			float z = event.values[2];
+			float ns = event.timestamp;
+			float deltat = ns - prevNs;
+			if (prevNs == 0) {
+				deltat = 0;
+			}
+			prevNs = ns;
+			if (running) {
+				JSONObject result = new JSONObject();
+				try {
+					result.put("type", type);
+					result.put("x", x);
+					result.put("y", y);
+					result.put("z", z);
+					result.put("deltat", deltat);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				out.println(result.toString());
+			}
+		}
+
 	}
 }
